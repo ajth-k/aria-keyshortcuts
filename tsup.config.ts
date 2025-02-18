c86505e3239d8901461ed3ba602a78fde4886700
Ajith Kumar P M <ajith@strollby.com>
2025-02-18 23:47:45 +0530

    chore: test release v0.0.0

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   clean: true,
   dts: true,
-  entry: ["src/index.ts"],
+  entry: ["src/{components,hooks}/**/index.{ts,tsx}"],
   format: ["esm"],
   sourcemap: true,
   minify: true,
